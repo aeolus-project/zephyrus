16948f0fd9e63af8276f437fabbf4b6987ffe848
Jakub Zwolakowski <jakub.zwolakowski@inria.fr>
2013-02-11 13:37:13 +0100

    Little cosmetic correction of the last commit.

--- typing_context.mli
+++ typing_context.mli
@@ -1,11 +1,11 @@
 
 open Aeolus_types_j
 
-val get_component_type : universe -> component_type_name -> component_type
+val get_component_type                : universe -> component_type_name -> component_type
 val get_component_type_implementation : universe -> component_type_name -> package_name list
-val get_repository : universe -> repository_name -> repository
-val get_package : repository -> package_name -> package
-val get_ports : universe -> port_name list
+val get_repository                    : universe -> repository_name -> repository
+val get_package                       : repository -> package_name -> package
+val get_ports                         : universe -> port_name list
 
 val requirers   : universe -> port_name -> component_type_name list
 val providers   : universe -> port_name -> component_type_name list
