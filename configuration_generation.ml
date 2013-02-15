289bc7bcad4656941d179a1d32ae27d2106ff8f4
Jakub Zwolakowski <jakub.zwolakowski@inria.fr>
2013-02-15 20:13:39 +0100

    Advanced in configuration generation. Having to take count of the old names when creating fresh names makes this thing a lot more complex...

--- configuration_generation.ml
+++ configuration_generation.ml
@@ -63,7 +63,145 @@ let generate_bindings (universe : universe) (components : component list) : bind
 
 open Facile_constraints
 
-let configuration_of_solution (universe : universe) (solution : solution) : configuration = 
+let solution_repository_for_location (solution : solution) (location_name : location_name) : repository_name =
+  let repository_names =
+    BatList.filter_map (fun (var_key, value) -> 
+      match var_key with    
+      | LocalRepositoryVariable (var_location_name, var_repository_name) ->
+          if (var_location_name = location_name) && (value = 1)
+          then Some var_repository_name
+      | _ -> None
+    ) solution
+  in
+  match repository_names with
+  | [repository_name] -> repository_name
+  | []                -> failwith (Printf.sprintf "in the solution no repository is assigned to location %s"                  location_name)
+  | _                 -> failwith (Printf.sprintf "in the solution there is more than one repository assigned to location %s" location_name)
+
+let solution_packages_for_location (solution : solution) (location_name : location_name) : package_name list =
+  let package_names =
+    BatList.filter_map (fun (var_key, value) -> 
+      match var_key with    
+      | LocalElementVariable (var_location_name, (Package var_package_name)) ->
+          if (var_location_name = location_name) && (value = 1)
+          then Some var_package_name
+      | _ -> None
+    ) solution
+  in
+  package_names
+
+let solution_number_of_components_for_location (solution : solution) (location_name : location_name) (component_type_name :component_type_name) : int =
+  let components_counts =
+    BatList.filter_map (fun (var_key, value) -> 
+      match var_key with    
+      | LocalElementVariable (var_location_name, (ComponentType var_component_type_name)) ->
+          if (var_location_name = location_name) && (var_component_type_name = component_type_name)
+          then Some value
+      | _ -> None
+    ) solution
+  in
+  match components_counts with
+  | [components_count] -> components_count
+  | []                 -> failwith (Printf.sprintf "in the solution number of components of type %s in location %s is unknown"                location_name component_type_name)
+  | _                  -> failwith (Printf.sprintf "in the solution number of components of type %s in location %s is defined more than once" location_name component_type_name)
+
+
+(*
+module type FRESH_COMPONENT_NAMES =
+  sig
+    type t
+    val fresh_component_name : t -> location_name -> component_type_name -> (t, component_type_name)
+  end
+*)
+
+module Fresh_component_names : FRESH_COMPONENT_NAMES =
+  struct
+    
+    type t = component_type_name list
+
+    let fresh_component_name (location_name : location_name) (component_type_name : component_type_name) (used_names : t) =
+      let fresh_name = Printf.sprintf "%s %s" location_name component_type_name
+      in
+      ( (fresh_name :: used_names), fresh_name )
+
+    let fresh_component_names (n : int) (location_name : location_name) (component_type_name : component_type_name) (used_names : t) =
+
+      let f (n : int) (used_names : t) (a : component_type_name list) =
+        match n with
+        | 0 -> (used_names, a)
+        | i -> let (new_used_names, fresh_name) = fresh_component_name location_name component_type_name used_names
+               in
+               f (n - 1) new_used_names (fresh_name :: a)
+
+      in 
+      f n used_names []
+
+  end
+
+let old_components_that_remain_for_location (initial_configuration : configuration) (solution : solution) (location_name : location_name) (component_type_name : component_type_name) : component list =
+  
+  let number_of_components =
+    solution_number_of_components_for_location solution location_name component_type_name
+  
+  and initial_configuration_components =
+    List.filter_map ( fun component ->
+      if (component.component_type = component_type_name) && (component.component_location = location_name)
+      then Some component.component
+      else None
+    ) initial_configuration.configuration_components
+
+  in
+
+  BatList.take number_of_components initial_configuration_component_names
+
+let old_components_that_remain (universe : universe) (initial_configuration : configuration) (solution : solution) : component list =
+  let location_names = get_location_names initial_configuration
+  and component_type_names = get_component_type_names universe
+  in
+  List.flatten ( List.flatten (
+    List.map (fun location_name ->
+      List.map (fun component_type_name ->
+        old_components_that_remain_for_location initial_configuration solution location_name component_type_name
+      ) component_type_names
+    ) location_names
+  ) )
+
+let new_components_for_location (initial_configuration : configuration) (solution : solution) (location_name : location_name) (component_type_name : component_type_name) (used_names : Fresh_component_names.t) : component list =
+  
+  let right_number_of_components =
+    solution_number_of_components_for_location solution location_name component_type_name
+  
+  and initial_configuration_components =
+    List.filter_map ( fun component ->
+      if (component.component_type = component_type_name) && (component.component_location = location_name)
+      then Some component.component
+      else None
+    ) initial_configuration.configuration_components
+  in
+
+  let number_of_initial_components = List.length initial_configuration_components
+  in
+
+  let fresh_component_names =
+    Fresh_component_names.fresh_component_names
+      (right_number_of_components - number_of_initial_components)
+      location_name
+      component_type_name
+      used_names
+
+  (* HERE *)
+
+let configuration_of_solution (universe : universe) (solution : solution) (initial_configuration : configuration) : configuration = 
+
+  let configuration_locations =
+    List.map (fun location -> {
+        location_name               : location.location_name;
+        location_provide_resources  : location.location_provide_resources;
+        location_repository         : solution_repository_for_location solution location.location_name;
+        location_packages_installed : solution_packages_for_location   solution location.location_name;
+      }
+    ) initial_configuration.configuration_locations
+
 
   let resources_list =
 
