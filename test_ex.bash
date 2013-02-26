0f228db4bb17fc46b6a6a84310622f27da156b8c
Jakub Zwolakowski <jakub.zwolakowski@inria.fr>
2013-02-26 15:21:21 +0100

    Working on the example...

--- test_ex.bash
+++ test_ex.bash
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-./gentlewestwind -u u_ex.json -spec spec_ex.spec -ic ic_ex.json -repo debian-squeeze repositories/repo-debian-squeeze.json -opt simple -print-cstrs -print-facile-vars
+./gentlewestwind\
+ -u u_ex.json\
+ -spec spec_ex.spec\
+ -ic ic_ex.json\
+ -repo debian-squeeze repositories/repo-debian-squeeze.json\
+ -opt simple\
+ -print-cstrs -print-facile-vars
